434f204e592ed6122e10643e2bb5b9bdb435369c
Dilukshan <pdiluksan@gmail.com>
2026-04-29 22:00:02 +0530

    resume card implemented

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,20 +1,34 @@
-import type { Route } from "./+types/home";
+import type {Route} from "./+types/home";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import {resume} from "react-dom/server";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "ResumeMate" },
-    { name: "description", content: "Smart feedback for your dream job!" },
-  ];
+    return [
+        {title: "ResumeMate"},
+        {name: "description", content: "Smart feedback for your dream job!"},
+    ];
 }
 
 export default function Home() {
-  return <main>
-  <section className="bg-[url('/images/bg-main.svg')] ">
-    <div className="page-heading">
-      <h1>Track your Applications & Resume Ratings</h1>
-      <h2>Review your submissions and check AI-powered feedback.</h2>
-    </div>
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar/>
+        <section className="main-section">
+            <div className="page-heading py-8">
+                <h1>Track your Applications & Resume Ratings</h1>
+                <h2>Review your submissions and check AI-powered feedback.</h2>
+            </div>
 
-  </section>
-  </main>
+
+            {resume.length > 0 && (
+                <div className="resumes-section">
+                    {resumes.map((resume) => (
+                        <ResumeCard key={resume.id} resume={resume}/>
+                    ))}
+                </div>
+            )}
+        </section>
+
+    </main>
 }
