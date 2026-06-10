6029097706bfc15261d1ddd9f2a3c174c196f563
Dilukshan <pdiluksan@gmail.com>
2026-06-10 20:09:46 +0530

    logout functioning in navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import {Link} from "react-router";
+import {usePuterStore} from "~/lib/puter";
 
 const Navbar = () => {
+    const {auth} = usePuterStore();
     return (
         <nav className="navbar">
             <Link to="/">
                 <p className="text-2xl font-bold text-gradient">RESUMEMATE</p>
             </Link>
-            <Link to="/upload" className="primary-button w-fit">
-               Upload Resume
-            </Link>
+            <div className="flex flex-row gap-4">
+                <Link to="/upload" className="primary-button w-fit">
+                    Upload Resume
+                </Link>
+                <button className="primary-button w-fit" onClick={auth.signOut}>
+                    Log Out
+                </button>
+            </div>
         </nav>
     );
 };
